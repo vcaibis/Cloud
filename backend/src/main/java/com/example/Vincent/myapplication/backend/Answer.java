6ec21d9c3937c6e95e15423560915777dbb16d19
Vincent <vcaibis@gmail.com>
2017-01-08 23:56:58 +0100

    Initial commit

---
+++ backend/src/main/java/com/example/Vincent/myapplication/backend/Answer.java
@@ -0,0 +1,58 @@
+package com.example.Vincent.myapplication.backend;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ * Created by Vincent on 03.11.2016.
+ */
+
+@Entity
+public class Answer implements Serializable{
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private static final long serialVersionUID = -7406082437623008161L;
+    long Id;
+    String content;
+    Question question;
+
+    public Answer() {
+
+    }
+
+    public Answer(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
---
+++ backend/src/main/java/com/example/Vincent/myapplication/backend/Question.java
@@ -0,0 +1,60 @@
+package com.example.Vincent.myapplication.backend;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ * Created by Vincent on 03.11.2016.
+ */
+
+@Entity
+public class Question implements Serializable{
+
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.IDENTITY
+    )
+    private static final long serialVersionUID = -7406082437623008161L;
+    long Id;
+    private String title;
+    private List<Answer> answers = new ArrayList();
+
+    public Question() {
+
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public Question(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
